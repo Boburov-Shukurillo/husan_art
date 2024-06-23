c62884fcc2ce226f980669a396057341255f218c
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2024-06-23 05:31:57 +0500

    server is finished

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const connectdb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`data base woring on ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
